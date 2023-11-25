23630dd07a9f19ea5f8f7cb61e6ad6aeca2ebd3d
Balotelli Mafo <xmafo1990@gmail.com>
2023-11-25 18:30:24 +0300

    java1.java

---
+++ java1.java
@@ -0,0 +1,6 @@
+public class SquareCalculator {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println("The square of " + num + " is " + num * num);
+    }
+}
